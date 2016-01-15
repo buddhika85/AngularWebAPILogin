9ce1f0cd76b4bdbfb4edf07e2e3c830590b3af49
buddhika85 <buddhika2006system@gmail.com>
2016-01-15 16:28:21 +0000

    Up to step 10 - http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/

--- AngularJSAuthentication.API/App_Start/WebApiConfig.cs
+++ AngularJSAuthentication.API/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace AngularJSAuthentication.API
@@ -9,7 +11,6 @@ namespace AngularJSAuthentication.API
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -19,6 +20,9 @@ namespace AngularJSAuthentication.API
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
